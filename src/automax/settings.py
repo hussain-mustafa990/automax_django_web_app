0f7e837e11931af1f1495a7d11beb2e753108090
Hussain Mustafa <hussain@preneure.com>
2022-07-03 11:53:22 -0400

    Fix static file issue

--- src/automax/settings.py
+++ src/automax/settings.py
@@ -62,7 +62,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
