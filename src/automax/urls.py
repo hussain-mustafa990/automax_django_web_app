a979bc0aa972ef97252af89fbccd6e9e7cb928cd
Hussain Mustafa <hussain@preneure.com>
2022-07-03 12:04:46 -0400

    Fix static file issue 3

--- src/automax/urls.py
+++ src/automax/urls.py
@@ -30,6 +30,3 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
-
-urlpatterns += static(settings.STATIC_URL,
-                      document_root=settings.STATIC_ROOT)
